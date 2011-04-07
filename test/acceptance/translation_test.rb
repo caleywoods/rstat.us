66ab99985e8ac09fbc2a3f265598e46784dc9596
Colin Dean <cad@cad.cx>
2011-04-07 19:11:46 -0400

    updated test

--- test/acceptance/translation_test.rb
+++ test/acceptance/translation_test.rb
@@ -4,18 +4,25 @@ require_relative 'acceptance_helper'
 class TranslationsTest < MiniTest::Unit::TestCase
 
   include AcceptanceHelper
-
   def test_translation_meta
     page.driver.header "Accept-Language", "en-US,en;q=0.8"
     visit "/"
     assert_match "<meta content='en' http-equiv='Content-Language' />", page.body
   end
+  def test_translation_meta_other
+    page.driver.header "Accept-Language", "ja,en-US;q=8.0,en;q=0.6"
+    visit "/"
+    assert_match "<meta content='ja' http-equiv='Content-Language' />", page.body
+  end
 
   def test_translation_language_detection
     #this might need to be updated when the front page changes
+    page.driver.reset!
     page.driver.header "Accept-Language", "eo,en-US;q=0.8,en;q=0.6"
     visit "/"
     assert_match "Komunigu", page.body
+    page.driver.reset!
+    page.driver.header "Accept-Language", "en-US,en;q=0.8"
   end
 
 end
